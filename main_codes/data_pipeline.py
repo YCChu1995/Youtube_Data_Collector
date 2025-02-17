1748d4d1403e2939357326b9e28207301440c3ca
YCChu1995 <chuyuchang1995@gmail.com>
2025-02-17 22:02:06 +0800

    debug

--- main_codes/data_pipeline.py
+++ main_codes/data_pipeline.py
@@ -4,11 +4,11 @@ from data_collector import DataCollector
 from utilities import load_configs
 
 if __name__ == '__main__':
-    data_collector = DataCollector('TW', show_progress_bar=True)
+    data_collector = DataCollector('TW', show_progress_bar=False)
 
     for interested_category in load_configs()['folder_name_to_save_data'].keys():
         data_collector.set_interest_categories(interested_category)
-        data_collector.collect_videos(max_videos=1, max_comments=1)
+        data_collector.collect_videos(max_videos=10, max_comments=100)
         data_collector.store_data()
         data_collector.clean_cached_data()
 
