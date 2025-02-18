932908e0c4006db163a886b8c846a5644bb3073e
YCChu1995 <chuyuchang1995@gmail.com>
2025-02-18 12:00:36 +0800

    Update data_collector.py

--- modules/data collector/data_collector.py
+++ modules/data collector/data_collector.py
@@ -24,7 +24,7 @@ class DataCollector:
         self.__youtube_crawler = YouTubeCrawler(location)
         self.__youtube_videos = []
         self.__show_progress_bar = show_progress_bar
-        self.__current_timestamp = datetime.datetime.now().astimezone(datetime.timezone(datetime.timedelta(hours=8))).strftime("%Y%m%d%H")
+        self.__current_timestamp = datetime.datetime.now().strftime("%Y%m%d%H")
         
         running_time: str = get_refined_time_string(time() - start_time)
         print(f"Data_Collector initialized in \033[92m{running_time}\033[0m !", end='\n', flush=True)
